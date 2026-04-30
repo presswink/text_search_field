fffef76a2c71df28375f003360973a67e60ae7a5
adityakumar <raj68518@gmail.com>
2026-04-30 10:45:44 +0530

    plugin to package converted

--- example/android/app/src/main/kotlin/com/presswink/text_search_field_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.presswink.text_search_field_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- test/src/utils_test.dart
+++ test/src/utils_test.dart
@@ -4,6 +4,6 @@ import 'package:text_search_field/src/Utils.dart';
 void main() {
   test("buildKey method Testing ", () {
     final res = Utils.buildKey("hello Bro");
-    expect(res, "hello_bro");
+    expect(res, "hellobro");
   });
 }
